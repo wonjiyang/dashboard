c7a3eaf03884bd8f2061618f2b0dd0d69bd76b22
wonjiyang <wonjiyang@gmail.com>
2024-01-29 15:38:21 +0900

    add

---
+++ src/component/layout/Header.jsx
@@ -0,0 +1,26 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Header</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">아우터</a>
+                    </li>
+                    <li>
+                        <a href="/">티셔츠</a>
+                    </li>
+                    <li>
+                        <a href="/">팬츠</a>
+                    </li>
+                    <li>
+                        <a href="/">악세사리</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/views/index.jsx
@@ -0,0 +1,10 @@
+import Layout from "../component/layout/Layout"
+
+const Home = () => {
+  return (
+    <Layout title="home">
+      메인 컨텐츠
+    </Layout>)
+  }
+
+export default Home
