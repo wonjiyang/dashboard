10a567bf5d780b66162e2a1ba90e4db60ffdb345
wonjiyang <wonjiyang@gmail.com>
2024-03-14 13:09:57 +0900

    add

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, VStack } from "@chakra-ui/react"
+import { Button, SimpleGrid, VStack } from "@chakra-ui/react"
 import MiniSt from "../../component/card/MiniSt"
 import Icon from "../../component/icons/Icon"
 import { MdOutlineBarChart } from "react-icons/md";
@@ -10,14 +10,38 @@ import DailyTraffic from "./components/DailyTraffic";
 import YourPieChart from "./components/YourPieChart";
 import TotalSpent from "./components/TotalSpent";
 import WeeklyRevenue from "./components/WeeklyRevenue";
-import ComplexTable from "./components/ComplexTable";
+// import ComplexTable from "./components/ComplexTable";
 import Task from "./components/Task";
 import MiniCalendar from "../../component/calendar/MiniCalendar";
 import ComplexTableComm from "./components/ComplexTableComm";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    // 컴포넌트가 마운팅될 때, 업데이트될 때, 언마운팅될 때 실행되는 useEffect
+    useEffect(() => {
+        // Mounting
+        document.body.style.backgroundColor = 'yellow'
+        document.body.id = 'intro'
+        document.body.classList.add('intro')
+        // Unmounting
+        return () => {
+            document.body.style.backgroundColor = ''
+            document.body.id = ''
+            document.body.classList.remove('intro')
+        }
+    }, [])
+    // Updating
+    const [color, setColor] = useState('yellow')
+    const toggleColor = () => {
+        setColor((prevColor) => (prevColor === 'yellow' ? 'lightblue' : 'yellow'))
+    }
+    useEffect(() => {
+        console.log('색상이 변경되었습니다' ,color)
+    }, [color])
+
     return (
         <VStack spacing={5}>
+            <Button onClick={toggleColor}>배경색 변경</Button>
             <SimpleGrid columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }} spacing="20px" w={'100%'}>
                 <MiniSt
                     name="Earnings"
